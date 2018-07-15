05f538c43c3cd942c360b6d2d75fbbf2d7303565
maojunwei <41253654+maojunwei@users.noreply.github.com>
2018-07-15 23:54:47 +0800

    斐波那契数列

---
+++ 斐波那契数列.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class solution
+{
+public:
+	int Fibonacci(int n) {
+        int a[39];
+        a[0] = 0;
+        a[1] = 1;
+        if(n >1)
+        {
+        for(int i =2;i<=n;i++)
+            a[i] = a[i-1] + a[i-2];
+        }
+        return a[n];
+    }
+
+};
+
