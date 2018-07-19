333d51a3f490451836902be87311bbaf2de7ad5d
maojunwei <41253654+maojunwei@users.noreply.github.com>
2018-07-19 14:24:15 +0800

    二叉树的下一个节点

--- 二叉树的下一个节点.cpp
+++ 二叉树的下一个节点.cpp
@@ -1,38 +1,37 @@
-#include<iostream>
-#include<stdlib.h>
-using namespace std;
-typedef struct bintree
-{
-	struct bintree *right;
-	struct bintree * left;
-	struct bintree *parent;
-	int data;
-}Ctree;
-Ctree* nextnode(Ctree* treenode)
-{
-	Ctree *p;
-	if(treenode == NULL)
-		return NULL;
-	if(treenode->right !=NULL )  //右子树不为空
-	{
-		p = treenode->right;
-		while(p->left != NULL)
-			p = p->left;
-		return p;	
-	}
-	else if(treenode->parent != NULL)//无右子树
-	{
-		if(treenode->parent->left == treenode)
-			return treenode->parent;
-		else
-		{
-			p = treenode;
-			while(p->parent != p->parent->parent->left)
-				p = p->parent;
-			return p->parent->parent;		
-		}
+/*
+题目：给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+        
+    }
+};
+*/
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        if(pNode == NULL)
+            return NULL;
+        if(pNode->right != NULL) //向下端
+        {
+            TreeLinkNode* p = pNode->right;
+            while(p->left != NULL)
+                p = p->left; 
+            return p;
+        }
+        while(pNode->next != NULL)  //向父结点遍历
+        {
+            TreeLinkNode* p = pNode->next;
+            if(p->left == pNode)
+                return p;
+            pNode = pNode->next;
 
-	}
-
-
-}
+        }
+        return NULL;
+         
+    }
+};
